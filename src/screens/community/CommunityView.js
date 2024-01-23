29e7be0a5f816d5a293036af43d883bd868af7fa
thuanvo09 <thuanvo09@hotmail.com>
2024-01-23 03:03:08 -0500

    redesign community view

--- src/screens/community/CommunityView.js
+++ src/screens/community/CommunityView.js
@@ -11,17 +11,33 @@ import {
     TouchableOpacity,
     Image,
     ImageBackground,
-    ScrollView
+    ScrollView,
+    RefreshControl,
 } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import CustomKeyboardAvoidingView from '../../components/CustomKeyboardAvoidingView'
 
 import sampleIcon from '../../../assets/images/sampleicon.jpg'
+import communityBg from '../../../assets/images/communitybg.jpg'
 
 const LoginScreen = () => {
+
+    const [refreshing, setRefreshing] = React.useState(false)
+    const onRefresh = React.useCallback(() => {
+        console.log('refreshing')
+    }, [])
+
+
     return (
         // <CustomKeyboardAvoidingView>
         <View className="flex h-screen w-screen">
             <ScrollView
+                refreshControl={
+                    <RefreshControl
+                        refreshing={refreshing}
+                        onRefresh={onRefresh}
+                    />
+                }
                 horizontal={false}
                 contentContainerStyle={{
                     display: 'flex',
@@ -33,7 +49,15 @@ const LoginScreen = () => {
                 className="h-screen w-screen overflow-auto bg-white"
                 onTouchStart={Keyboard.dismiss}
             >
-                <View className="mb-5 flex h-fit w-full flex-col items-center justify-center rounded-3xl bg-orchid-300 py-12 shadow-md shadow-orchid-300">
+                <ImageBackground
+                    source={communityBg}
+                    resizeMode="cover"
+                    imageStyle={{
+                        borderRadius: 24,
+                    }}
+                    blurRadius={5}
+                    className="mb-5 flex h-fit w-full flex-col items-center justify-center rounded-3xl  py-12 shadow-md shadow-orchid-300"
+                >
                     <View className="mb-5 flex h-fit w-28 items-center justify-center rounded-full bg-transparent shadow shadow-orchid-600">
                         <Image
                             source={sampleIcon}
@@ -41,9 +65,21 @@ const LoginScreen = () => {
                         />
                     </View>
 
-                    <View className="mb-5 flex flex-grow items-center justify-center">
-                        <Text className="mb-1 text-2xl font-semibold text-white shadow shadow-orchid-600">
-                            Community Name
+                    <View className="mb-5 flex items-center justify-center">
+                        <View className="mb-1 flex flex-row gap-2">
+                            <Text className="text-2xl font-semibold text-white shadow shadow-orchid-600">
+                                Community Name
+                            </Text>
+                            <TouchableOpacity>
+                                <Ionicons
+                                    name="create-outline"
+                                    size={26}
+                                    color={'#fff'}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        <Text className="mb-1 text-base text-white shadow shadow-orchid-600">
+                            Host by ILUVUS
                         </Text>
                         <View className="flex flex-row items-center justify-center gap-5">
                             <Text className="text-base text-white shadow shadow-orchid-600">
@@ -57,23 +93,23 @@ const LoginScreen = () => {
 
                     <View className="flex flex-row items-center justify-center gap-5">
                         <TouchableOpacity className="flex h-fit w-fit flex-row flex-wrap items-center justify-center rounded-full bg-white px-5 py-2 shadow shadow-orchid-600">
-                            <Text className="text-lg text-orchid-900">
+                            <Text className="text-md text-orchid-900">
                                 JOIN US
                             </Text>
                         </TouchableOpacity>
                         <TouchableOpacity className="flex h-fit w-fit flex-row flex-wrap items-center justify-center rounded-full bg-white px-5 py-2 shadow shadow-orchid-600">
-                            <Text className="text-lg text-orchid-900">
+                            <Text className="text-md text-orchid-900">
                                 VIEW POSTS
                             </Text>
                         </TouchableOpacity>
                     </View>
-                </View>
+                </ImageBackground>
 
                 <View className="mb-5 flex h-fit w-full flex-col items-start justify-start rounded-3xl bg-white p-5 shadow-md shadow-slate-300">
                     <Text className="mb-2 text-2xl font-bold text-orchid-900">
                         Description
                     </Text>
-                    <Text className="text-base text-orchid-900">
+                    <Text className="text-base text-orchid-800">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                         Sed at risus at erat pretium bibendum. Sed eget
                         vestibulum eros. Aenean sed lobortis mi, vitae
