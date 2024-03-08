48f393c09e97af7a1018dca1a8f89217f1044e34
thuanvo09 <thuanvo09@hotmail.com>
2024-03-08 03:44:47 -0500

    modify notification
    each notification has:
    
    dateTime: '2021-10-10T00:00:00',
                type: 'comment',
                postId: '541',
                userId: 'D',
                communityId: '333',
                userFullName: 'User F',
                communityName: 'Community 22',

--- src/screens/Notification/Notification.js
+++ src/screens/Notification/Notification.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text } from 'react-native'
 import { ScrollView } from 'react-native-gesture-handler'
 import { useState } from 'react'
@@ -7,45 +7,95 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faUserTag } from '@fortawesome/free-solid-svg-icons'
 import COLORS from '../../constants/colors'
 import Notitem from './Notitem'
+import axios from 'axios'
+import { BASE_URL } from '@env'
 
 const Notification = () => {
     const [userId, setUserId] = useState('Thuan')
     const [notifications, setNotifications] = useState([
-        'A tagged you in a post',
-        'B tagged you in a post',
-        'C tagged you in a post',
-        'D tagged you in a post',
-        'E tagged you in a post',
-        'F tagged you in a post',
-        'G tagged you in a post',
-        'H tagged you in a post',
-        'I tagged you in a post',
-        'J tagged you in a post',
-        'K tagged you in a post',
-        'L tagged you in a post',
-        'M tagged you in a post',
-        'N tagged you in a post',
-        'O tagged you in a post',
-        'P tagged you in a post',
+        {
+            dateTime: '2021-10-10T00:00:00',
+            type: 'tag',
+            postId: '123',
+            userId: 'A',
+            communityId: '333',
+            userFullName: 'User A',
+            communityName: 'Community A',
+        },
+        {
+            dateTime: '2021-10-10T00:00:00',
+            type: 'report',
+            postId: '123',
+            userId: 'B',
+            communityId: '311',
+            userFullName: 'User X',
+            communityName: 'Community A',
+        },
+        {
+            dateTime: '2021-10-10T00:00:00',
+            type: 'like',
+            postId: '143',
+            userId: 'C',
+            communityId: '322',
+            userFullName: 'User T',
+            communityName: 'Community G',
+        },
+        {
+            dateTime: '2021-10-10T00:00:00',
+            type: 'comment',
+            postId: '541',
+            userId: 'D',
+            communityId: '333',
+            userFullName: 'User F',
+            communityName: 'Community 22',
+        },
     ])
+
+    useEffect(() => {
+        fetchNotifications()
+    }, [])
+
+    const fetchNotifications = () => {
+        axios({
+            method: 'GET',
+            url: `${BASE_URL}/user/${userId}`,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((res) => {
+                setNotifications(res.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
     return (
         <View className="w-screenflex-1 flex h-screen bg-white">
-            <View className="h-full w-full">
-                <ScrollView
-                    contentContainerStyle={{
-                        paddingBottom: 250,
-                        flexGrow: 1,
-                    }}
-                    className="overflow-ao h-full w-full space-y-3 bg-transparent p-5"
-                >
-                    {notifications.map((value, index) => (
-                        <View className="flex h-fit w-full flex-row items-start justify-start rounded-3xl bg-orchid-100 p-5 shadow-sm shadow-slate-300">
-                            <Notitem key={index} text={value} />
-                        </View>
-                    ))}
-                </ScrollView>
-            </View>
+            {notifications.length > 0 && (
+                <View className="h-full w-full">
+                    <ScrollView
+                        contentContainerStyle={{
+                            paddingBottom: 250,
+                            flexGrow: 1,
+                        }}
+                        className="overflow-ao h-full w-full space-y-3 bg-transparent p-5"
+                    >
+                        {notifications.map((data, index) => (
+                            <View className="flex h-fit w-full flex-row items-start justify-start rounded-3xl bg-orchid-100 p-5 shadow-sm shadow-slate-300">
+                                <Notitem key={index} data={data} />
+                            </View>
+                        ))}
+                    </ScrollView>
+                </View>
+            )}
+
+            {notifications.length <= 0 && (
+                <View className="flex h-full w-full items-center justify-start p-5">
+                    <Text>You don't have any notifications.</Text>
+                </View>
+            )}
         </View>
     )
 }
--- src/screens/Notification/Notitem.js
+++ src/screens/Notification/Notitem.js
@@ -7,18 +7,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
 import { faUserTag } from '@fortawesome/free-solid-svg-icons'
 import COLORS from '../../constants/colors'
 
-const Notitem = ({text}) => {
+const NotitemCaseMsg = ({ data }) => {
+    switch (data.type) {
+        case 'tag':
+            return `${data.userFullName} tagged you in a post in ${data.communityName}`
+        case 'report':
+            return `${data.userFullName} reported your post in ${data.communityName}`
+        case 'like':
+            return `${data.userFullName} liked your post in ${data.communityName}`
+        case 'comment':
+            return `${data.userFullName} commented on your post in ${data.communityName}`
+    }
+}
+
+const Notitem = ({ data }) => {
     return (
-        <>
+        <View className="mr-14 flex flex-row items-start justify-start space-x-4">
             <FontAwesomeIcon
                 icon={faUserTag}
                 color={COLORS.orchid[700]}
                 size={30}
             />
-            <Text className="items-start justify-start pl-5 text-orchid-900 shadow">
-                <Text>{text}</Text>
-            </Text>
-        </>
+            <View className="flex flex-col">
+                <Text className="mb-2 text-xs text-orchid-700">
+                    {data.dateTime}
+                </Text>
+                <Text
+                    className="items-start justify-start text-orchid-900 shadow"
+                    numberOfLines={2}
+                >
+                    <NotitemCaseMsg data={data} />
+                </Text>
+            </View>
+        </View>
     )
 }
 export default Notitem
